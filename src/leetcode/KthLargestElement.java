c8ea5de230e05c02d92972126b85f8c0138c1dfa
the7crownless <the7crownless@github>
2023-07-18 21:56:09 +0800

    215. 数组中的第K个最大元素

--- src/leetcode/KthLargestElement.java
+++ src/leetcode/KthLargestElement.java
@@ -1,7 +1,54 @@
 package leetcode;
 
+import java.util.Random;
+
+//快排的思想
+
+//循环不变量的新思想
 public class KthLargestElement {
+    private int k;
+    private Random random = new Random();
+
     public int findKthLargest(int[] nums, int k) {
-        return 0;
+        this.k = nums.length - k;
+        return quickSort(nums, 0, nums.length - 1);
+
+    }
+
+    private int quickSort(int[] nums, int left, int right) {
+        int pivot = partition(nums, left, right);
+        if (pivot == k) {
+            return nums[pivot];
+        } else if (pivot > k) {
+            return quickSort(nums, left, pivot - 1);
+        } else {
+            return quickSort(nums, pivot + 1, right);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+
+        int pivot = random.nextInt(right - left + 1) + left;
+        int x = nums[pivot];
+        swap(nums,pivot,right);
+        //初始地方在left右侧
+        int j = left-1;
+        //循环不到right处，即保证不会交换导致pivot的值改变
+        for (int i = left; i < right; i++) {
+            if (nums[i] < x) {
+                j++;
+                swap(nums, j, i);
+            }
+        }
+
+        //right保存的就是pivot的值
+        swap(nums,j,right);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
